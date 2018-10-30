38893305639fa9c335e32b0928d9dd62c6ba0783
federico_caccia <federicoagustincaccia@gmail.com>
2018-10-30 16:52:54 -0300

    changing default settings

--- settings.py
+++ settings.py
@@ -1,53 +1,37 @@
-exchanges = ["Binance", 
-             "Bittrex",
-             "Bitfinex",
-             "Okex",
-             "Poloniex",
-             "Kraken",
-             "Cex"]
-exchanges = ["Binance"]
-symbols = [
-    # Binance
-    "ADA/BTC"
-    # cex
-    "BTC/USD",
-    "ETH/USD",
-    "BCH/USD",
-    "BCT/EUR",
-    "ETH/BTC",
-    "BCH/BTC",
-    # kraken
-    "BTC/EUR",
-    "ETC/USD",
-    "ETC/EUR",
-    "ETC/BTC",
-    "ETC/ETH",
-    # "REP/EUR",
-    # "REP/BTC",
-    # "REP/ETH",
-    "XMR/USD",
-    "XMR/EUR",
-    "XMR/BTC",
-    # okex
-    "EOS/BTC",
-    # "IOST/BTC",
-    # "QTUM/BTC",
-    "BCT/USDT",
-    "LTC/USDT",    
-    "ETH/USDT",
-    "ETC/USDT",
-    "BCH/USDT",
-    "EOS/USDT",
-    "XRP/USDT",
-    # bitfinex
-    "LTC/USD",
-    "ETH/USD",
-    "ETC/USD",
-    "BCH/USD",
-    "EOS/USD",
-    "XRP/USD"
+exchanges = [
+    "Binance",
+    "Bittrex",
+    "Bitfinex"
 ]
-
-symbols = ["BCH/USDT",
-           "LOOM/BTC"]
+exchanges = ["Binance", "Bitfinex"]
+symbols = [
+    "BTC/USD",
+    "BTC/USDT",
+    "EHT/BTC",
+    "XRP/BTC",
+    "BCH/BTC",
+    "EOS/BTC",
+    "XLM/BTC",
+    "LTC/BTC",
+    "ADA/BTC",
+    "XMR/BTC",
+    "TRX/BTC",
+    "DASH/BTC",
+    "IOTA/BTC",
+    "BNB/BTC",
+    "NEO/BTC",
+    "ETC/BTC",
+    "NEM/BTC",
+    "XTZ/BTC",
+    "ZEC/BTC",
+    "VEN/BTC",
+    "MKR/BTC",
+    "BTG/BTC",
+    "DOGE/BTC",
+    "OMG/BTC",
+    "ZRX/BTC",
+    "LOOM/BTC",
+    "ENJ/BTC",
+    "MIT/BTC"
+]
 database_type = "csv" # "csv" - "sql" - "nosql"
